ee88dd106a879d2ebab4e9789d6982b15784e6b3
Rizki008 <muhammadrizkinurhamdani@gmail.com>
2023-07-04 16:39:55 +0700

    update library login user dan pelangga

--- application/libraries/Login_user.php
+++ application/libraries/Login_user.php
@@ -6,58 +6,56 @@ class Login_user
 {
 	protected $ci;
 
-	// public function __construct()
-	// {
-	// 	$this->ci = &get_instance();
-	// 	$this->ci->load->model('m_auth');
-	// }
+	public function __construct()
+	{
+		$this->ci = &get_instance();
+		$this->ci->load->model('m_auth');
+	}
 
-	// public function login($email, $password)
-	// {
-	// 	$cek = $this->ci->m_auth->login_pelanggan($email, $password);
-	// 	if ($cek) {
-	// 		$id_pelanggan = $cek->id_pelanggan;
-	// 		$nama_pelanggan = $cek->nama_pelanggan;
-	// 		$email = $cek->email;
-	// 		$no_telpon = $cek->no_telpon;
-	// 		$jenis_kel = $cek->jenis_kel;
-	// 		$alamat = $cek->alamat;
-	// 		$foto = $cek->foto;
+	public function login($username, $password)
+	{
+		$cek = $this->ci->m_auth->login_user($username, $password);
+		if ($cek) {
+			$id_pelanggan = $cek->id_pelanggan;
+			$nama_user = $cek->nama_user;
+			$username = $cek->username;
+			$password = $cek->password;
+			$level = $cek->level;
 
-	// 		//session
-	// 		$this->ci->session->set_userdata('id_pelanggan', $id_pelanggan);
-	// 		$this->ci->session->set_userdata('nama_pelanggan', $nama_pelanggan);
-	// 		$this->ci->session->set_userdata('email', $email);
-	// 		$this->ci->session->set_userdata('no_telpon', $no_telpon);
-	// 		$this->ci->session->set_userdata('jenis_kel', $jenis_kel);
-	// 		$this->ci->session->set_userdata('alamat', $alamat);
-	// 		$this->ci->session->set_userdata('foto', $foto);
+			//session
+			$this->ci->session->set_userdata('id_pelanggan', $id_pelanggan);
+			$this->ci->session->set_userdata('nama_user', $nama_user);
+			$this->ci->session->set_userdata('username', $username);
+			$this->ci->session->set_userdata('password', $password);
+			$this->ci->session->set_userdata('level', $level);
 
-	// 		redirect('home');
-	// 	} else {
-	// 		$this->ci->session->set_flashdata('error', 'email Atau Password Salah');
-	// 		redirect('pelanggan/login');
-	// 	}
-	// }
+			if ($level == 1) {
+				redirect('admin');
+			} elseif ($level == 2) {
+				redirect('pemilik');
+			}
+		} else {
+			$this->ci->session->set_flashdata('error', 'username Atau Password Salah');
+			redirect('admin/login');
+		}
+	}
 
-	// public function proteksi_halaman()
-	// {
-	// 	if ($this->ci->session->userdata('nama_pelanggan') == '') {
-	// 		$this->ci->session->set_flashdata('error', 'Anda Belum Login');
-	// 		redirect('pelanggan/login');
-	// 	}
-	// }
+	public function proteksi_halaman()
+	{
+		if ($this->ci->session->userdata('username') == '') {
+			$this->ci->session->set_flashdata('error', 'Anda Belum Login');
+			redirect('admin/login');
+		}
+	}
 
-	// public function logout()
-	// {
-	// 	$this->ci->session->unset_userdata('id_pelanggan');
-	// 	$this->ci->session->unset_userdata('nama_pelanggan');
-	// 	$this->ci->session->unset_userdata('email');
-	// 	$this->ci->session->set_userdata('no_telpon');
-	// 	$this->ci->session->set_userdata('jenis_kel');
-	// 	$this->ci->session->set_userdata('alamat');
-	// 	$this->ci->session->unset_userdata('foto');
-	// 	$this->ci->session->set_flashdata('pesan', 'Berhasil LogOut!!!!');
-	// 	redirect('pelanggan/login');
-	// }
+	public function logout()
+	{
+		$this->ci->session->unset_userdata('id_pelanggan');
+		$this->ci->session->unset_userdata('nama_user');
+		$this->ci->session->unset_userdata('username');
+		$this->ci->session->set_userdata('password');
+		$this->ci->session->set_userdata('level');
+		$this->ci->session->set_flashdata('pesan', 'Berhasil LogOut!!!!');
+		redirect('admin/login');
+	}
 }
